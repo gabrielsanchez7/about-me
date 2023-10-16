ff8f0fef08888937311fbe79b234ca8352571fe5
gsanchez <gabriel.sanchezv27@gmail.com>
2023-10-15 22:03:30 -0500

    feat: project structure and header web mode

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import HeaderBg from "~images/head-bg.png"
+import Header from "./sections/header/Header.vue"
+</script>
+
+<template>
+  <Header :style="{backgroundImage: `url(${HeaderBg})`}"/>
+</template>
---
+++ src/sections/header/Header.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Logo from "~images/logo.png"
+</script>
+
+<template>
+  <div class="header">
+    <div class="container">
+      <a class="header__logo" href="javascript:void(0)">
+        <img :src=Logo alt="Logo">
+      </a>
+      <ul class="header__menu">
+        <li class="header__menu-item"><a href="javascript:void(0)">Inicio</a></li>
+        <li class="header__menu-item"><a href="javascript:void(0)">Sobre m&iacute;</a></li>
+        <li class="header__menu-item"><a href="javascript:void(0)">Portafolio</a></li>
+        <li class="header__menu-item"><a href="javascript:void(0)">Experiencia y estudios</a></li>
+        <li class="header__menu-item"><a href="javascript:void(0)">Contacto</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style>
+@import url("./header.css");
+</style>
